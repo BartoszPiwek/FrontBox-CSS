1012425ee3768abe29cad9573eaab68d9cb4d556
Bartosz Piwek <bartosz.piwek@gogomedia.pl>
2018-10-19 18:35:17 +0200

    Working

--- src/js/frontbox/cookies.js
+++ src/js/frontbox/cookies.js
@@ -1,21 +1,49 @@
-cookies: function() {
+module.exports = (data) => {
 
-    if (!Cookies.get('cookies_information')) {
-        var content = 
-        `
-            <div class="js_cookies-information" id="js_cookies-information">
-                <div class="wrap">
-                    <div class="js_cookies-information__container">
-                        <img src="/images/cookies.png" class="js_cookies-information__img" alt="Ciasteczka">
-                        <p class="js_cookies-information__text">
-                            Nasza strona internetowa używa plików cookies (tzw. ciasteczka) w celach statystycznych, reklamowych oraz funkcjonalnych. Dzięki nim możemy indywidualnie dostosować stronę do twoich potrzeb. Każdy może zaakceptować pliki cookies albo ma możliwość wyłączenia ich w przeglądarce, dzięki czemu nie będą zbierane żadne informacje.
-                        </p>
-                        <p class="js_cookies-information__exit js_cookies-close"></p>
+    const
+    ELEMENTS = data.ELEMENTS;
+
+    let
+    OPTIONS = {
+        content: null,
+        imgSrc: '/assets/images/cookies.png',
+    };
+
+    const
+    start = (data) => {
+
+        if (!Cookies.get('cookies_information')) {
+            $.extend( OPTIONS, data.OPTIONS );
+            show();
+        }
+
+    };
+
+    const
+    createElement = () => {
+
+        return  `
+                <div class="js_cookies-information" id="js_cookies-information">
+                    <div class="wrap">
+                        <div class="js_cookies-information__container">
+                            <img src="${OPTIONS.imgSrc}" class="js_cookies-information__img" alt="Ciasteczka">
+                            <p class="js_cookies-information__text">
+                                ${OPTIONS.content}
+                            </p>
+                            <p class="js_cookies-information__exit js_cookies-close"></p>
+                        </div>
                     </div>
                 </div>
-            </div>
-        `;
-        $( "#body" ).append( $( content ) );
+                `;
+    };
+
+    const
+    show = (data) => {
+        // Create element
+        let element = createElement();
+        // Append to $body
+        ELEMENTS.$body.append( $( element ) );
+
         var $cookie = $("#js_cookies-information"),
             $exit = $(".js_cookies-close");
         var close = function(){
@@ -23,6 +51,9 @@ cookies: function() {
             $cookie.addClass("js_cookies-information--hide");
         };
         $exit.on("click", close);
-    }
 
-},
+    };
+
+    start(data);
+
+};
