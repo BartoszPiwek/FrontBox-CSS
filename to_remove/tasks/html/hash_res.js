60e6ab7cbc10077f06761b05fbcb5ce6211f5a34
bpiwek <bartosz.piwek@gogomedia.pl>
2019-06-04 22:31:59 +0200

    LESS -> SCSS

--- to_remove/tasks/html/hash_res.js
+++ to_remove/tasks/html/hash_res.js
@@ -4,38 +4,38 @@
 
 module.export = {
 
-    css: {
-        options: {
-            resDir: 'public/prod',
-            resReg: [
-                /(images\/[^.]+\.(?:png|gif|jpg))(\??)/g,
-                // /(fonts\/[^.]+\.[\w]+)(\??)/g
-            ],
-            force: true,
-        },
-        files: [{
-            expand: true,
-            cwd: 'public/prod/css',
-            src: ['**/*.css'],
-            dest: 'public/prod/css'
-        }]
-    },
-    html: {
-        options: {
-            resDir: 'public/prod',
-            force: true,
-            resReg: [
-                /(images\/[^.]+\.(?:png|gif|jpg|ico))(\??)/g,
-                /(css\/[^.]+\.(?:css))(\??)/g,
-                /(js\/[^.]+\.(?:min.js))(\??)/g
-            ]
-        },
-        files: [{
-            expand: true,
-            cwd: 'public/prod',
-            src: ['*.html'],
-            dest: 'public/prod'
-        }]
-    }
+	css: {
+		options: {
+			resDir: 'public/prod',
+			resReg: [
+				/(images\/[^.]+\.(?:png|gif|jpg))(\??)/g,
+				// /(fonts\/[^.]+\.[\w]+)(\??)/g
+			],
+			force: true,
+		},
+		files: [{
+			expand: true,
+			cwd: 'public/prod/css',
+			src: ['**/*.css'],
+			dest: 'public/prod/css'
+		}]
+	},
+	html: {
+		options: {
+			resDir: 'public/prod',
+			force: true,
+			resReg: [
+				/(images\/[^.]+\.(?:png|gif|jpg|ico))(\??)/g,
+				/(css\/[^.]+\.(?:css))(\??)/g,
+				/(js\/[^.]+\.(?:min.js))(\??)/g
+			]
+		},
+		files: [{
+			expand: true,
+			cwd: 'public/prod',
+			src: ['*.html'],
+			dest: 'public/prod'
+		}]
+	}
 
 };
